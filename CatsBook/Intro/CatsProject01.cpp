90e79f0487db962e26e62c8bcf546a309a761f94
Oleksandr Karasov <68482964+OleksandrKarasovCorp@users.noreply.github.com>
2021-08-18 18:16:23 +0300

    First commit

---
+++ CatsBook/Intro/CatsProject01.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::cout << "Input values for a, b, c, d, please: " << '\n';
+
+    double a;
+	std::cin >> a;
+
+	double b;
+	std::cin >> b;
+
+	double c;
+	std::cin >> c;
+
+	double d;
+	std::cin >> d;
+
+	double halfSum = (a + b + c + d) / 2;
+	
+	std::cout << "The half-sum of inputted values: " << halfSum << '\n';
+
+	double halfDifference = (a - b - c - d) / 2;
+
+	std::cout << "The half-difference of inputted values: " << halfDifference << '\n';
+
+	double maxValue = 0;
+
+	if (a > b) {
+		maxValue = a;
+	}
+	else {
+		maxValue = b;
+	}
+	if (maxValue < c) {
+		maxValue = c;
+	}
+	if (maxValue < d) {
+		maxValue = d;
+	}
+
+	std::cout << "Max value is: " << maxValue << '\n';
+
+	double minValue = 0;
+
+	if (a < b) {
+		minValue = a;
+	}
+	else {
+		minValue = b;
+	}
+	if (minValue > c) {
+		minValue = c;
+	}
+	if (minValue > d) {
+		minValue = d;
+	}
+
+	std::cout << "Min value is: " << minValue << '\n';
+
+	double productOfMaxAndMin = maxValue * minValue;
+
+	std::cout << "The product of max and min value: " <<productOfMaxAndMin<< '\n';
+
+	return 0;
+}
+
+
+
+
