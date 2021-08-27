4bfc5306b07a5da7277377402bd2247d093d1dca
Oleksandr Karasov <68482964+OleksandrKarasovCorp@users.noreply.github.com>
2021-08-27 22:47:16 +0300

    Add files via upload

---
+++ CatsBook/Intro/cats_task03_b.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+
+int main()
+{  
+
+	std::cout << "Input value of x, please " << '\n';
+	double x;
+	std::cin >> x;
+
+	// Horner's method 
+
+	double firstExpression = -1 + x * (-8 + x * x * (2 + 5 * x));
+
+	std::cout << firstExpression << '\n';
+
+	double secondExpression = 6 + (x * (-5 + x * x * (2 * x - 3 + 4 * x)));
+
+	std::cout << secondExpression << '\n';
+
+	return 0;
+}
+
