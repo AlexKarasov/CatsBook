d7e60e2054399913d2c2aacd4f18005b157a17ac
Oleksandr Karasov <68482964+OleksandrKarasovCorp@users.noreply.github.com>
2021-08-28 20:42:03 +0300

    Create cats_task08.cpp

---
+++ CatsBook/Intro/cats_task08.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+
+int main()
+{  
+	std::cout << "Input the value of a" << std::endl;
+	int a3;
+	std::cin >> a3;
+
+	a3 = a3 * a3 * a3;
+	
+	std::cout << "The value of a = " << a3 << std::endl;
+
+	std::cout << "Input the value of a" << std::endl;
+	int a10;
+	std::cin >> a10;
+
+	a10 = a10 * a10;
+	int b10 = a10 * a10; 
+	int c10 = b10 * b10;
+	int d10 = c10 * a10;
+
+	std::cout << "The value of a = " << d10 << std::endl;
+
+	std::cout << "Input the value of a" << std::endl;
+	int a4;
+	std::cin >> a4;
+
+	a4 = a4 * a4;
+	a4 = a4 * a4;
+	
+	std::cout << "The value of a = " << a4 << std::endl;
+
+	std::cout << "Input the value of a" << std::endl;
+	int a20;
+	std::cin >> a20;
+
+	a20 = a20 * a20;
+	int b20 = a20 * a20;
+	int c20 = b20 * b20;
+	int d20 = c20 * c20;
+	a20 = d20 * b20;
+
+	std::cout << "The value of a = " << a20 << std::endl;
+
+	std::cout << "Input the value of a" << std::endl;
+	int a5;
+	std::cin >> a5;
+
+	a5 = a5 * a5 * a5 * a5 * a5;
+
+	std::cout << "The value of a = " << a5 << std::endl;
+
+	std::cout << "Input the value of a" << std::endl;
+	int a13;
+	std::cin >> a13;
+
+	int aa13 = a13 * a13;
+	int b13 = aa13 * aa13;
+	int c13 = b13 * b13;
+	int d13 = c13 * b13;
+	a13 = d13 * a13;
+
+	std::cout << "The value of a = " << a13 << std::endl;
+
+	std::cout << "Input the value of a" << std::endl;
+	int a2;
+	std::cin >> a2;
+
+	a2 = a2 * a2;
+
+	std::cout << "The value of a = " << a2 << std::endl;
+
+	std::cout << "Input the value of a" << std::endl;
+	int a51;
+	std::cin >> a51;
+
+	int aa5 = a51 * a51 * a51 * a51 * a51;
+
+	std::cout << "The value of a = " << aa5 << std::endl;
+
+	std::cout << "Input the value of a" << std::endl;
+	int a17;
+	std::cin >> a17;
+
+	int aa17 = a17 * a17;
+	int b17 = aa17 * aa17;
+	int c17 = b17 * b17;
+	int d17 = c17 * c17;
+	int e17 = d17 * a17;
+
+	std::cout << "The value of a = " << e17 << std::endl;
+
+	std::cout << "Input the value of a" << std::endl;
+	int a41;
+	std::cin >> a41;
+
+	a41 = a41 * a41;
+	int b41 = a41 * a41;
+	
+	std::cout << "The value of a = " << b41 << std::endl;
+
+	std::cout << "Input the value of a" << std::endl;
+	int a12;
+	std::cin >> a12;
+
+	a12 = a12 * a12;
+	int b12 = a12 * a12;
+	int c12 = b12 * b12;
+	int d12 = c12 * c12;
+	int e12 = c12 * b12;
+
+	std::cout << "The value of a = " << e12 << std::endl;
+
+	std::cout << "Input the value of a" << std::endl;
+	int a28;
+	std::cin >> a28;
+
+	a28 = a28 * a28;
+	int b28 = a28 * a28;
+	int c28 = b28 * b28;
+	int d28 = c28 * c28;
+	int e28 = d28 * b28;
+	a28 = e28 * c28;
+
+	std::cout << "The value of a = " << a28 << std::endl;
+
+	return 0;
+}
+
+
