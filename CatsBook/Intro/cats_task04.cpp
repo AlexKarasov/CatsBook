6826e216f25e2992e677197e9d14e9e8e0bf1144
Oleksandr Karasov <68482964+OleksandrKarasovCorp@users.noreply.github.com>
2021-08-28 09:55:45 +0300

    Add files via upload

---
+++ CatsBook/Intro/cats_task04.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+
+int main()
+{  
+	double v1;
+	std::cout << "Input the value of volume V1: " << std::endl;
+	std::cin >> v1;
+
+	double t1;
+	std::cout << "Input the value of temperature t1: " << std::endl;
+	std::cin >> t1;
+
+	double v2;
+	std::cout << "Input the value of volume V2: " << std::endl;
+	std::cin >> v2;
+
+	double t2;
+	std::cout << "Input the value of temperature t2: " << std::endl;
+	std::cin >> t2;
+
+	double v3 = v1 + v2;
+	std::cout << "The volume of V3: " << v3 << std::endl;
+
+	double t3 = (v1*t1+v2*t2)/ v3;
+	std::cout << "The temperature of t3: " << t3 << std::endl;
+
+	return 0;
+}
+
